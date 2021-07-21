44ba292e509406dcbb850e06c002082e32efd16f
kirx76 <ekirichenk8@gmail.com>
2021-07-21 09:06:15 +0300

    prod test

--- components/main/introduce/Introduce.tsx
+++ components/main/introduce/Introduce.tsx
@@ -1,24 +1,36 @@
-import React, {Component} from "react";
-import styles from './Introduce.module.scss'
+import React, { Component } from "react";
+import styles from "./Introduce.module.scss";
 
 export default class Introduce extends Component<any, any> {
   render() {
     return (
       <div className={styles.Introduce}>
         <div className={styles.Introduce_Image}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
-            src='https://sun9-22.userapi.com/impf/c830109/v830109662/d6b1b/B86haq6vdz0.jpg?size=2560x1702&quality=96&sign=5e52ec059e6edbf4e6e5902d0676ebc5&type=album'
-            alt="my photo"/>
+            src="https://sun9-22.userapi.com/impf/c830109/v830109662/d6b1b/B86haq6vdz0.jpg?size=2560x1702&quality=96&sign=5e52ec059e6edbf4e6e5902d0676ebc5&type=album"
+            alt="my photo"
+          />
         </div>
         <div className={styles.Introduce_Block}>
           <div className={styles.Introduce_Block_Top}>
-            <span>I’m Kirx. A Front-end developer <span className={styles.Introduce_Block_Top_Base}>based in Kazan.</span></span>
+            <span>
+              I’m Kirx. A Front-end developer{" "}
+              <span className={styles.Introduce_Block_Top_Base}>
+                based in Kazan.
+              </span>
+            </span>
           </div>
           <div className={styles.Introduce_Block_Bottom}>
-            <span> I’m probably the most passionate «Fronter» you will ever get to work with. If you have a great project that needs some amazing skills, I’m your guy.</span>
+            <span>
+              {" "}
+              I’m probably the most passionate «Fronter» you will ever get to
+              work with. If you have a great project that needs some amazing
+              skills, I’m your guy.
+            </span>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
--- components/main/philosophy/Philosophy.tsx
+++ components/main/philosophy/Philosophy.tsx
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
-import styles from './Philosophy.module.scss';
-import img from '/public/img.png'
+import React, { Component } from "react";
+import styles from "./Philosophy.module.scss";
+import img from "/public/img.png";
 
 export default class Philosophy extends Component<any, any> {
   render() {
@@ -11,16 +11,22 @@ export default class Philosophy extends Component<any, any> {
             <span>Philosophy & values</span>
           </div>
           <div className={styles.Philosophy_Left_Content}>
-            <span>I think everyone wants the same thing - relationship with humanity, peace with the metaphysical, and experience with the universe. I try to grasp these things with my values: authenticity, creativity, & hospitality.</span>
+            <span>
+              I think everyone wants the same thing - relationship with
+              humanity, peace with the metaphysical, and experience with the
+              universe. I try to grasp these things with my values:
+              authenticity, creativity, & hospitality.
+            </span>
           </div>
           <div className={styles.Philosophy_Left_Link}>
             <span>More about me</span>
           </div>
         </div>
         <div className={styles.Philosophy_Right}>
-          <img src={img.src} alt="Philosophy"/>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={img.src} alt="Philosophy" />
         </div>
       </div>
-    )
+    );
   }
 }
