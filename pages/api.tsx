3361a4c8ac5ce526f394a17dbfb255d126abeca5
kirx76 <kirx76>
2021-07-24 23:09:44 +0300

    test api cause

---
+++ pages/api.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import RootStore from "stores/RootStore";
+
+export default class ApiPage extends Component<RootStore, any> {
+  render() {
+    return <div>test api, may be crush</div>;
+  }
+}
